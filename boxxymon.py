e5ccba54c7e1624d355b75fb353e424ec4f801db
wamde <wamde.dev@gmail.com>
2019-01-04 21:16:21 +0000

    Sorting channels by age (using chan_id)

--- boxxymon.py
+++ boxxymon.py
@@ -3,6 +3,10 @@ import rpc_pb2 as ln, rpc_pb2_grpc as lnrpc
 
 from os.path import expanduser
 
+import curses
+import time
+
+
 LND_DIR = expanduser("~/.lnd")
 macaroon = codecs.encode(open(LND_DIR + '/data/chain/bitcoin/mainnet/admin.macaroon', 'rb').read(), 'hex')
 os.environ['GRPC_SSL_CIPHER_SUITES'] = 'HIGH+ECDSA'
@@ -18,7 +22,35 @@ channel = grpc.secure_channel('localhost:10009', ssl_creds, channel_options)
 stub = lnrpc.LightningStub(channel)
 
 
+
+
 def main():
+
+	global channel_graph
+	channel_graph = get_channel_graph()
+
+	stdscr = curses.initscr()
+	curses.noecho()
+	curses.cbreak()
+
+	try:
+		while True:
+			stdscr.addstr(0, 0, get_header_line())
+			channel_line = 2
+			for c in get_channels():
+				stdscr.addstr(channel_line, 0, c.encode("UTF-8"))
+				channel_line += 1
+			stdscr.refresh()
+			time.sleep(10)
+	finally:
+		curses.echo()
+		curses.nocbreak()
+		curses.endwin()
+		for c in get_channels():
+			print c
+
+
+def get_header_line():
 
 	# header
 	response = stub.GetInfo(ln.GetInfoRequest(), metadata=[('macaroon', macaroon)])
@@ -26,24 +58,32 @@ def main():
 	chain = response.chains[0]
 	version = response.version
 
-	channel_graph = get_channel_graph()
-
 	header = "%s (%s, lnd %s)" % (alias, chain, version)
 
-	print header
-
-
-	# channels
 	request = ln.ListChannelsRequest(
 		active_only = True
 		)
 	response = stub.ListChannels(request, metadata=[('macaroon', macaroon)])
 	channels = response.channels
 
+	header += "\n"+("%d active channels" % (len(channels)))
+	return header
+
+def get_channels():
+
+	# channels
+	request = ln.ListChannelsRequest(
+		active_only = True
+		)
+	response = stub.ListChannels(request, metadata=[('macaroon', macaroon)])
+	channels_ = response.channels
+
+	channels = sorted(channels_, key=lambda c: int(c.chan_id), reverse=False)
+
 	channel_number = 0
 	channels_with_names = {}
 
-	print "%d active channels" % (len(channels))
+	#print "%d active channels" % (len(channels))
 	max_capacity = max([c.capacity for c in channels])
 	for channel in channels:
 		channels_with_names[string.ascii_uppercase[channel_number]] = {"channel": channel}
@@ -53,12 +93,12 @@ def main():
 		remote_pubkey = channel.remote_pubkey
 		node_alias = get_node_alias(channel_graph, remote_pubkey)
 		if len(node_alias) >= 15:
-			node_alias = node_alias[:9] + "[...]"
+			node_alias = node_alias[:13] + "/"
 		else:
 			node_alias += " "*(15-1-len(node_alias))
 		score = channel_score(capacity, local_balance)
                 channels_with_names[string.ascii_uppercase[channel_number]]["score"] = score
-		print (string.ascii_uppercase[channel_number] + "/ " + str(chan_id) + " "
+		yield (string.ascii_uppercase[channel_number] + "/ " + str(chan_id) + " "
 			   + node_alias + " "
 			   + channel_cursor(capacity, local_balance, max_capacity, score)
 			   + " " + remote_pubkey)
@@ -68,7 +108,7 @@ def channel_cursor(capacity, local_balance, max_capacity, score):
 	max_capacity_digits = len("{:,}".format(max_capacity))
 	num_steps = 60
 	num_pluses = int(num_steps * float(local_balance) / float(capacity))
-	cursor_string = "[" + "+"*num_pluses + "-"*(num_steps-num_pluses) + "]"
+	cursor_string = "+"*num_pluses + "-"*(num_steps-num_pluses)
 	balance_string = "(%d/%d sat)" % (local_balance, capacity)
 	balance_string = "({local_balance:>{max_capacity_digits},} / {capacity:>{max_capacity_digits},}), score={score:.3f}".format(
 		local_balance=local_balance, capacity=capacity,
